3d9cd45045d9b86cc382df2c4fddae57e89083aa
Bailey <baileypiggott@gmail.com>
2015-07-30 10:06:24 -0400

    discipline specific engineering CAT boxplots

--- CAT_boxplots_eng_by_discipline.R
+++ CAT_boxplots_eng_by_discipline.R
@@ -1,54 +1,69 @@
+# engineering CAT boxplots by discipline
 
-# CAT boxplots
+source("setup_CAT_eng_disciplines.R")
 
-source("setup_CAT.R") #RUN THIS EVERY TIME! ENGINEERING SUBJECT NAME IS CHANGED FOR THE DISCIPLINE BARPLOTS AND WILL BE OUT OF ORDER
 
 # choose what to plot
-df <- dram
-prog_name <- "Drama"
-dummy <- dummy_4 %>% mutate(Subject = Queens) #all 4th year data is drama
+df <- mech
+prog_name <- "Mechanical Engineering"
 
-df <- psyc
-prog_name <- "Psychology"
-dummy <- dummy_4 %>% mutate(Subject = PSYC) 
+df <- elec
+prog_name <- "Electrical Engineering"
 
-df <- phys
-prog_name <- "Physics"
-dummy <- dummy_4 %>% mutate(Subject = PHYS) 
+df <- cmpe
+prog_name <- "Computer Engineering"
 
-df <- apsc
-prog_name <- "Engineering"
-dummy <- dummy_4 %>% mutate(Subject = APSC) 
+df <- civl
+prog_name <- "Civil Engineering"
 
+df <- chee
+prog_name <- "Chemical Engineering"
+
+df <- ench
+prog_name <- "Engineering Chemistry"
+
+df <- mine
+prog_name <- "Mining Engineering"
+
+df <- geoe
+prog_name <- "Geological Engineering"
+
+df <- enph
+prog_name <- "Engineering Physics"
+
+df <- mthe
+prog_name <- "Math and Engineering"
+
+# RUN FROM HERE--------------------------------------------------------------------------
 
 #calculate sample sizes:
 n_1 <-  sum(with(df, year == 1 & CAT.Score > 1), na.rm = TRUE)     
-year1 <- paste0("First Year\nn = ", n_1, "   n = ", n_q_1) #text string for xlabel including sample size
+year1 <- paste0("First Year\nn = ", n_1, "   n = ", n_eng_1) #text string for xlabel including sample size
 n_2 <-  sum(with(df, year ==2 & CAT.Score > 1), na.rm = TRUE)     
-year2 <- paste0("Second Year\nn = ", n_2, "   n = ", n_q_2) #text string for xlabel
+year2 <- paste0("Second Year\nn = ", n_2, "   n = ", n_eng_2) #text string for xlabel
 n_3 <-  sum(with(df, year == 3 & CAT.Score > 1), na.rm = TRUE)     
-year3 <- paste0("Third Year\nn = ", n_3, "   n = ", n_q_3) #text string for xlabel
+year3 <- paste0("Third Year\nn = ", n_3, "   n = ", n_eng_3) #text string for xlabel
 n_4 <-  sum(with(df, year == 4 & CAT.Score > 1), na.rm = TRUE)     
-year4 <- paste0("Fourth Year\nn = ", n_4, "   n = ", 0) #text string for xlabel
+year4 <- paste0("Fourth Year\nn = ", n_4, "   n = ", n_eng_4 #text string for xlabel
 
 # set up data frame and title
-df <- rbind(df, queens, fix) # combine with all queens data
+df <- rbind(df, eng, fix) # combine with all queens data
 graph_title <- paste0(prog_name,  " CAT Scores")
 
 ## plot description
 ggplot(
-    data = df, 
-    aes(x = as.factor(year), y = CAT.Score, fill = Subject)
+  data = df, 
+  aes(x = as.factor(year), y = CAT.Score, fill = Subject)
   ) +
   geom_boxplot(
     width = 0.5
-  ) + # geom_boxplot must be after stat_boxplot    
+    ) +     
   coord_cartesian(xlim = c(0.5,4.5),ylim = c(0, 40)) +
   scale_x_discrete(labels = c(year1, year2, year3, year4)) + #text strings from above with sample sizes
   labs(title = graph_title, x = "Year", y = "CAT Score")  +
   scale_fill_manual(
     values =  c("darkgoldenrod1", "steelblue3"),
-    labels = c(prog_name, "Queen's")
+    labels = c(prog_name, "All Engineering")
   )+
   theme(
     panel.border = element_rect(colour = "grey", fill = NA), #add border around graph
